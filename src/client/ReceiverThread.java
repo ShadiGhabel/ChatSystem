c582cb1ba87543c3db108af642c55d9a234df230
ShadiGhabel <ghabelshadi@gmail.com>
2026-01-02 17:44:27 +0330

    ADD EXPORT & TESTED

--- src/client/ReceiverThread.java
+++ src/client/ReceiverThread.java
@@ -4,6 +4,8 @@ import network.*;
 import common.Message;
 import common.ExportData;
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ReceiverThread implements Runnable {
     private ObjectInputStream in;
@@ -69,6 +71,10 @@ public class ReceiverThread implements Runnable {
                 }
                 break;
 
+            case EXPORT_RES:
+                handleExportResponse((ExportData) packet.getPayload());
+                break;
+
             case ERROR:
                 System.out.println("[!] Error: " + packet.getPayload());
                 break;
@@ -76,5 +82,61 @@ public class ReceiverThread implements Runnable {
             default:
                 System.out.println("Server: " + packet.getPayload());
         }
+    }
+
+    private void handleExportResponse(ExportData exportData) {
+        try {
+            StringBuilder json = new StringBuilder();
+            json.append("{\n");
+
+            json.append("  \"room\": \"").append(escapeJson(exportData.getRoomName())).append("\",\n");
+
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            String timestamp = dateFormat.format(new Date(exportData.getExportedAt()));
+            json.append("  \"exportedAt\": \"").append(timestamp).append("\",\n");
+
+            json.append("  \"messages\": [\n");
+
+            for (int i = 0; i < exportData.getMessages().size(); i++) {
+                Message msg = exportData.getMessages().get(i);
+                json.append("    {\n");
+                json.append("      \"id\": \"").append(escapeJson(msg.getId())).append("\",\n");
+                json.append("      \"sender\": \"").append(escapeJson(msg.getSender())).append("\",\n");
+                json.append("      \"timestamp\": ").append(msg.getTimestamp()).append(",\n");
+                json.append("      \"type\": \"").append(msg.getType()).append("\",\n");
+                json.append("      \"content\": \"").append(escapeJson(msg.getContent())).append("\"\n");
+                json.append("    }");
+
+                if (i < exportData.getMessages().size() - 1) {
+                    json.append(",");
+                }
+                json.append("\n");
+            }
+
+            json.append("  ]\n");
+            json.append("}\n");
+
+            try (FileWriter writer = new FileWriter(exportSavePath)) {
+                writer.write(json.toString());
+                System.out.println("\n[✓] Export successful!");
+                System.out.println("    Saved to: " + exportSavePath);
+                System.out.println("    Messages: " + exportData.getMessages().size());
+                System.out.println("    Room: " + exportData.getRoomName());
+            }
+
+        } catch (IOException e) {
+            System.out.println("[!] Export failed: " + e.getMessage());
+        }
+    }
+
+    private String escapeJson(String str) {
+        if (str == null) {
+            return "";
+        }
+        return str.replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\t", "\\t");
     }
 }
